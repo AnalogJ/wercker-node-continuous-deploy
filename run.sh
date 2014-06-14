dae3fa04a965b3b108ab49d29d19054a94b95fdc
jason <jk17@ualberta.ca>
2014-06-14 11:51:56 -0700

    added docs

--- run.sh
+++ run.sh
@@ -61,11 +61,12 @@ elif [ "$WERCKER_NODE_CONTINUOUS_DEPLOY_DEPLOY_BRANCH" = "$WERCKER_GIT_BRANCH" ]
     #task1 npm publish
     if [ -n "$WERCKER_NODE_CONTINUOUS_DEPLOY_NPM_PUBLISH" ]; then
         echo "Generating .npmrc file"
-        touch ~/.npmrc
-        echo "_auth = $WERCKER_NODE_CONTINUOUS_DEPLOY_NPM_AUTH" >> ~/.npmrc
-        echo "email = $WERCKER_NODE_CONTINUOUS_DEPLOY_NPM_EMAIL" >> ~/.npmrc
+        touch .npmrc
+        echo "_auth = $WERCKER_NODE_CONTINUOUS_DEPLOY_NPM_AUTH" >> .npmrc
+        echo "email = $WERCKER_NODE_CONTINUOUS_DEPLOY_NPM_EMAIL" >> .npmrc
         echo "Publishing package"
-        npm publish
+        cat .npmrc
+        npm publish --verbose
     fi
 
 else
